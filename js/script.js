552e6ab66dd3938a9142773fd07aaf3c15bc3b85
frontendgirl03 <ayupovadiana03@gmail.com>
2024-10-03 16:55:27 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+// alert("Hello Javascript");
+
+// const place = confirm("Are you here?");
+// console.log(place);
+
+// const currentPlace = prompt("Where are you?", "Diana");
+// console.log(currentPlace);
+
+// const age = +prompt("How old are you?", "21");
+// console.log(typeof age);
+
+// const favouriteColor = [];
+
+// favouriteColor[0]= prompt("What's your favourite color #1", "");
+// favouriteColor[1]= prompt("What's your favourite color #2", "");
+// favouriteColor[2]= prompt("What's your favourite color #3", "");
+// favouriteColor[3]= prompt("What's your favourite color #4", "");
+// console.log(favouriteColor);
+
+// const channel = prompt("What's your favourite youtuber?");
+// console.log(`https://youtube.com/${channel}/videos`);
+
+// const user = "Diana";
+// const job = "Javascript developer";
+
+// console.log(`User name is ${user}, she is ${job}`);
+
+// let incr = 10;
+// let dicr = 10;
+
+// console.log(++incr);
+// console.log(--dicr);
+
+// console.log(++incr);
+// console.log(--dicr);
+
+// console.log(incr);
+// console.log(dicr);
+
+const isAge = true;
+const isClose =true;
+const isChecked = false;
+
+console.log(isAge && isClose && isChecked);
+
+console.log(isAge || isClose || isChecked);
+
